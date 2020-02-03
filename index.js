48286b3daf948ddcae554dd6921f48c6f2ca98ad
David Lee <dlee195@live.com>
2020-02-02 20:13:00 -0500

    playing with canvas clickablility and footer

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let rect = canvas.getBoundingClientRect();
 
+const MAX_RIPPLE_SIZE = 300;
+
 // Setup canvas resizing
 function initCanvas() {
     canvas.height = window.innerHeight;
@@ -12,42 +14,54 @@ function initCanvas() {
 initCanvas();
 window.addEventListener("resize", initCanvas);
 
-// Setup waterdrop animation
-let drawInterval;
-function drawRipple(rip) {
-    console.log("RUNNING :)");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.strokeStyle= rip.color;
-    ctx.arc(
-        (rip.x - rect.left) / (rect.right - rect.left) * canvas.width,
-        (rip.y - rect.top) / (rect.bottom - rect.top) * canvas.height,
-        rip.r,
-        0,
-        2 * Math.PI
-    );
-    ctx.stroke();
+// Helper function to generate color rgb string
+function generateColor(t) {
+    return `rgba(255, 255, 255, ${t})`
+}
 
-    rip.r += rip.delta;
-    if(rip.r > 2500) {
-        clearInterval(drawInterval);
+// Helper to create a ripple object
+function initRipple(x, y, r, d) {
+    return {
+        x: x,
+        y: y,
+        r: r,
+        delta: d
     }
 }
 
-let ripple = {
-    color: 'rgb(255,255,255)',
-    x: 0,
-    y: 0,
-    r: 0,
-    delta: 0
+// Setup waterdrop animation
+let drawInterval;
+function drawRipples(rips) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = rips.length -1; i >= 0; i--) {
+        ctx.beginPath();
+        // Transitions the color from 255 to 0
+        ctx.strokeStyle = generateColor((Math.max(MAX_RIPPLE_SIZE - (rips[i].r), 0)/MAX_RIPPLE_SIZE));
+        ctx.arc(
+            (rips[i].x - rect.left) / (rect.right - rect.left) * canvas.width,
+            (rips[i].y - rect.top) / (rect.bottom - rect.top) * canvas.height,
+            rips[i].r,
+            0,
+            2 * Math.PI
+        );
+        ctx.stroke();
+
+        rips[i].r += rips[i].delta;
+
+        // Only check the most recent ripple
+        if (rips[i].r > MAX_RIPPLE_SIZE) {
+            rips.splice(i,1);
+            if (rips.length == 0) {
+                clearInterval(drawInterval);
+            }
+        }
+    }
 }
 
-canvas.addEventListener('click', (e) => {
-    ripple.x = e.clientX;
-    ripple.y = e.clientY;
-    ripple.r = 1;
-    ripple.delta = 1;
+let ripples = []
 
+canvas.addEventListener('click', (e) => {
+    ripples.unshift(initRipple(e.clientX, e.clientY, 1, 1));
     clearInterval(drawInterval);
-    drawInterval = setInterval(() => drawRipple(ripple), 1);
+    drawInterval = setInterval(() => drawRipples(ripples), 5);
 });
