19b7fae992ff221e7dcc2e2b99326ad965e4abd5
David Lee <dlee195@live.com>
2020-02-08 00:05:30 -0500

    adding basic navbar and auto rain

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-// Canvas Variables
+// Constants
+const MAX_RIPPLE_SIZE = 300;
+const RAIN_ENABLED_CLASS = "fas fa-tint";
+const RAIN_DISABLED_CLASS = "fas fa-tint-slash";
+
+// HTML Element Variables and friends
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let rect = canvas.getBoundingClientRect();
+const rainToggle = document.getElementById("rain-toggle")
 
-const MAX_RIPPLE_SIZE = 300;
+// State Variables
+let isRaining = false;
 
-// Setup canvas resizing
+//Helper Functions
+// Reset Canvas size to window
 function initCanvas() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
     rect = canvas.getBoundingClientRect();
 }
-initCanvas();
-window.addEventListener("resize", initCanvas);
-
 // Helper function to generate color rgb string
 function generateColor(t) {
     return `rgba(255, 255, 255, ${t})`
 }
-
 // Helper to create a ripple object
-function initRipple(x, y, r, d) {
+function initRipple(x, y) {
     return {
         x: x,
         y: y,
-        r: r,
-        delta: d
+        r: 1,
+        delta: 1
     }
 }
+// Create a ripple somewhere in the canvas
+function createRandomRipple() {
+    return initRipple(
+        Math.floor(Math.random() * canvas.width),
+        Math.floor(Math.random() * canvas.height)
+    );
+}
+
+// Here we go
+initCanvas();
+window.addEventListener("resize", initCanvas);
 
 // Setup waterdrop animation
 let drawInterval;
 function drawRipples(rips) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = rips.length -1; i >= 0; i--) {
+    for (var i = rips.length - 1; i >= 0; i--) {
         ctx.beginPath();
         // Transitions the color from 255 to 0
-        ctx.strokeStyle = generateColor((Math.max(MAX_RIPPLE_SIZE - (rips[i].r), 0)/MAX_RIPPLE_SIZE));
+        ctx.strokeStyle = generateColor((Math.max(MAX_RIPPLE_SIZE - (rips[i].r), 0) / MAX_RIPPLE_SIZE));
         ctx.arc(
             (rips[i].x - rect.left) / (rect.right - rect.left) * canvas.width,
             (rips[i].y - rect.top) / (rect.bottom - rect.top) * canvas.height,
@@ -50,18 +65,35 @@ function drawRipples(rips) {
 
         // Only check the most recent ripple
         if (rips[i].r > MAX_RIPPLE_SIZE) {
-            rips.splice(i,1);
+            rips.splice(i, 1);
             if (rips.length == 0) {
-                clearInterval(drawInterval);
+                if (isRaining) {
+                    rips.push(createRandomRipple());
+                } else {
+                    clearInterval(drawInterval);
+                }
             }
         }
     }
 }
 
 let ripples = []
-
 canvas.addEventListener('click', (e) => {
-    ripples.unshift(initRipple(e.clientX, e.clientY, 1, 1));
+    ripples.unshift(initRipple(e.clientX, e.clientY));
     clearInterval(drawInterval);
     drawInterval = setInterval(() => drawRipples(ripples), 5);
 });
+rainToggle.addEventListener('click', () => {
+    if (isRaining) {
+        ripples.length = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        clearInterval(drawInterval);
+        rainToggle.className = RAIN_DISABLED_CLASS;
+    } else {
+        ripples.push(createRandomRipple());
+        rainToggle.className = RAIN_ENABLED_CLASS;
+        clearInterval(drawInterval);
+        drawInterval = setInterval(() => drawRipples(ripples), 5);
+    }
+    isRaining = !isRaining;
+})
