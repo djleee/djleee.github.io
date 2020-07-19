8e2505d2ae7d8965f7d1c7850eace0abb393c93c
David Lee <dlee195@live.com>
2020-07-19 01:19:30 -0400

    testing moon parallax

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@
 const MAX_RIPPLE_SIZE = 300;
 const RAIN_ENABLED_CLASS = "fas fa-tint";
 const RAIN_DISABLED_CLASS = "fas fa-tint-slash";
+const MOON_ENABLED_CLASS = "fas fa-moon";
+const MOON_DISABLED_CLASS = "far fa-moon";
 
 // HTML Element Variables and friends
 const canvas = document.getElementById('canvas');
@@ -9,20 +11,26 @@ const ctx = canvas.getContext('2d');
 let rect = canvas.getBoundingClientRect();
 const rainToggle = document.getElementById("rain-toggle");
 const rainToggleIcon = document.getElementById("rain-toggle-icon");
+const moonToggle = document.getElementById("moon-toggle");
+const moonToggleIcon = document.getElementById("moon-toggle-icon");
+
+const homeCanvas = document.getElementById("homeCanvas");
+const homeCtx = homeCanvas.getContext('2d');
 
 // State Variables
 let isRaining = false;
-
-// Cursor Click Location for fish following
-let clickX = -1;
-let clickY = -1;
+let isMoonEnabled = true;
 
 //Helper Functions
 // Reset Canvas size to window
-function initCanvas() {
+function initCanvases() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
     rect = canvas.getBoundingClientRect();
+
+    homeCanvas.height = window.innerHeight;
+    homeCanvas.width = window.innerWidth;
+    drawMoon(0, 0);
 }
 // Helper function to generate color rgb string
 function generateColor(t) {
@@ -46,8 +54,8 @@ function createRandomRipple() {
 }
 
 // Here we go
-initCanvas();
-window.addEventListener("resize", initCanvas);
+initCanvases();
+window.addEventListener("resize", initCanvases);
 
 // Setup waterdrop animation
 let drawInterval;
@@ -98,65 +106,35 @@ rainToggle.addEventListener('click', () => {
         rainToggleIcon.className = RAIN_ENABLED_CLASS;
     }
     isRaining = !isRaining;
-})
-
-// FISH?
-function drawFish(X, Y, quarterLength, halfWidth, rotation) {
-    let cosRotation = Math.cos(rotation);
-    let sinRotation = Math.sin(rotation);
-
-    let origin          = { x: Math.floor(X                                                           ) , y: Math.floor(Y                                                           ) };
-    let lCurveUpper     = { x: Math.floor(X + (halfWidth*sinRotation)                                 ) , y: Math.floor(Y + (halfWidth*cosRotation)                                 ) };
-    let rCurveUpper     = { x: Math.floor(X - (halfWidth*sinRotation)                                 ) , y: Math.floor(Y - (halfWidth*cosRotation)                                 ) };
-    let lCurveLower     = { x: Math.floor(X - (2*quarterLength*cosRotation) + (halfWidth*sinRotation) ) , y: Math.floor(Y + (2*quarterLength*sinRotation) + (halfWidth*cosRotation) ) };
-    let rCurveLower     = { x: Math.floor(X - (2*quarterLength*cosRotation) - (halfWidth*sinRotation) ) , y: Math.floor(Y + (2*quarterLength*sinRotation) - (halfWidth*cosRotation) ) };
-    let tailConnection  = { x: Math.floor(X - (3*quarterLength*cosRotation)                           ) , y: Math.floor(Y + (3*quarterLength*sinRotation)                           ) };
-    let lTail           = { x: Math.floor(X + (halfWidth*sinRotation) - (4*quarterLength*cosRotation) ) , y: Math.floor(Y + (4*quarterLength*sinRotation) + (halfWidth*cosRotation) ) };
-    let rTail           = { x: Math.floor(X - (halfWidth*sinRotation) - (4*quarterLength*cosRotation) ) , y: Math.floor(Y + (4*quarterLength*sinRotation) - (halfWidth*cosRotation) ) };
-
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.moveTo(tailConnection.x, tailConnection.y);
-    ctx.lineTo(lTail.x, lTail.y);
-    ctx.lineTo(rTail.x, rTail.y);
-    ctx.lineTo(tailConnection.x,tailConnection.y);
-
-    ctx.bezierCurveTo(lCurveLower.x, lCurveLower.y, lCurveUpper.x, lCurveUpper.y, origin.x, origin.y);
-    ctx.bezierCurveTo(rCurveUpper.x, rCurveUpper.y,rCurveLower.x, rCurveLower.y, tailConnection.x, tailConnection.y);
-    ctx.fill();
-
-}
-
-let x = 300;
-let y = 500;
-let fishRotation = 0;
-function drawFishes() {
-    let speed = 5;
-
-    let diffX = clickX - x;
-    let diffY = y - clickY;
-
-    if (clickX > 0 && clickY > 0) {
-        if (Math.abs(diffX) < 3 && Math.abs(diffY) < 3) {
-            clickX = -1;
-            clickY = -1;
-        } else {
-            fishRotation = Math.atan(diffY/diffX)
-            if ( clickX <= x ) {
-                fishRotation += Math.PI;
-            }
-            x += speed * Math.cos(fishRotation);
-            y -= speed * Math.sin(fishRotation);
-        }
+});
+moonToggle.addEventListener('click', () => {
+    if (isMoonEnabled) {
+        moonToggleIcon.className = MOON_DISABLED_CLASS;
+    } else {
+        moonToggleIcon.className = MOON_ENABLED_CLASS;
     }
-    drawFish(x, y, 50, 40, fishRotation);
+    isMoonEnabled = !isMoonEnabled;
+});
+
+// Home Canvas
+function drawMoon(xSway, ySway) {
+    homeCtx.clearRect(0, 0, homeCanvas.width, homeCanvas.height);
+    homeCtx.fillStyle = "white";
+    homeCtx.beginPath();
+    homeCtx.arc((homeCanvas.width / 2) + xSway, homeCanvas.height / 4, 100, 0, 2 * Math.PI);
+    homeCtx.fill();
 }
+
+homeCanvas.addEventListener("mousemove", (e) => {
+    if (isMoonEnabled) {
+        drawMoon(((canvas.width / 2) - e.clientX) * 0.5, e.clientY);
+    }
+});
 
 function animateLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.lineWidth = 2;
     drawRipples(ripples);
-    drawFishes();
 
     requestAnimationFrame(animateLoop);
 }
