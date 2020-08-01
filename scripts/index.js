55137e999167a60823df1f546bea4bbcc64a7b37
David Lee <dlee195@live.com>
2020-08-01 18:58:04 -0400

    adding moon phases

--- scripts/index.js
+++ scripts/index.js
@@ -15,10 +15,10 @@ const moonToggle = document.getElementById("moon-toggle");
 const moonToggleIcon = document.getElementById("moon-toggle-icon");
 
 const homeDiv = document.getElementById("home");
-const moon = document.getElementById("moon");
-const moon2 = document.getElementById("moon2");
+const moon = document.getElementById("moon-group");
+const moonOverlayCircle = document.getElementById("moon-overlay-circle");
+const moonOverlayRectangle = document.getElementById("moon-overlay-rectangle");
 const fish = document.getElementById("fish");
-const fish2 = document.getElementById("fish2");
 
 // State Variables
 let isRaining = false;
@@ -125,15 +125,11 @@ moonToggle.addEventListener('click', () => {
     if (isMoonEnabled) {
         moonToggleIcon.className = MOON_DISABLED_CLASS;
         moon.style = "display: none";
-        moon2.style = "display: none;perspective: 100px";
         fish.style = "display: ";
-        fish2.style = "display: ";
     } else {
         moonToggleIcon.className = MOON_ENABLED_CLASS;
         moon.style = "display: ";
-        moon2.style = "display: ";
         fish.style = "display: none";
-        fish2.style = "display: none";
     }
     isMoonEnabled = !isMoonEnabled;
     drawMoons(0, 0);
@@ -144,10 +140,8 @@ moonToggle.addEventListener('click', () => {
 function drawMoons(xSway, ySway) {
     if (isMoonEnabled) {
         moon.style = "transform: translate(" + xSway + "px, " + ySway / 10 + "px)";
-        moon2.style = "transform: translate(" + xSway + "px, " + ySway / -10 + "px)";
     } else {
         fish.style = "transform: translate(" + (2 * homeDiv.clientWidth / 5 + xSway) + "px, " + ((homeDiv.clientHeight / 5) + ySway / 10) + "px) scale(0.5, 0.5)";
-        fish2.style = "transform: translate(" + (3 * homeDiv.clientWidth / 5 + xSway) + "px, " + ((7 * homeDiv.clientHeight / 8) + ySway / -10) + "px) scale(-0.5, -0.35)";
     }
 }
 
@@ -173,7 +167,7 @@ animateLoop();
 // Workplace gallery
 let expandedDiv = document.createElement("div");
 for (let item of document.getElementsByClassName("workplace-card")) {
-    if(expandedDiv.parentNode) {
+    if (expandedDiv.parentNode) {
         expandedDiv.parentNode.removeChild(expandedDiv);
     }
     item.addEventListener('click', () => {
@@ -182,3 +176,47 @@ for (let item of document.getElementsByClassName("workplace-card")) {
         item.append(expandedDiv);
     });
 }
+
+// Update moon phase!
+// Original Snippet: https://gist.github.com/endel/dfe6bb2fbe679781948c
+const MoonOverlayCircleStyles = [
+    "r: 9.5%", // new-moon
+    "transform: translateX(-5%)",   // waxing-crescent-moon
+    "display: none",                // quarter-moon
+    "transform: translateX(-10%)",  // waxing-gibbous-moon
+    "display: none",                // full-moon
+    "transform: translateX(10%)",   // waning-gibbous-moon
+    "display: none",                // last-quarter-moon
+    "transform: translateX(5%)",    // waning-crescent-moon
+]
+const MoonOverlayRectangleStyles = [
+    "display: none",                            // new-moon
+    "display: none",                            // waxing-crescent-moon
+    "x: 0; y: 0; height: 100%; width: 50%",     // quarter-moon
+    "display: none",                            // waxing-gibbous-moon
+    "display: none",                            // full-moon
+    "display: none",                            // waning-gibbous-moon
+    "x: 50%; y: 0; height: 100%; width: 50%",   // last-quarter-moon
+    "display: none",                            // waning-crescent-moon
+]
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate()
+let c = e = jd = b = 0;
+if (month < 3) {
+    year--;
+    month += 12;
+}
+++month;
+c = 365.25 * year;
+e = 30.6 * month;
+jd = c + e + day - 694039.09; // jd is total days elapsed
+jd /= 29.5305882; // divide by the moon cycle
+b = parseInt(jd); // int(jd) -> b, take integer part of jd
+jd -= b; // subtract integer part to leave fractional part of original jd
+b = Math.round(jd * 8); // scale fraction from 0-8 and round
+if (b >= 8) b = 0; // 0 and 8 are the same so turn 8 into 0
+
+moonOverlayCircle.style = MoonOverlayCircleStyles[b];
+moonOverlayRectangle.style = MoonOverlayRectangleStyles[b];
